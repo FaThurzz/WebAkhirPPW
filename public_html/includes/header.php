41c2fb42128b56e13b44c82b526f399eff16689e
FaThurzz <faturyk833@gmail.com>
2026-06-06 17:38:06 +0700

    dashboard users

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -230,7 +230,7 @@ $base_url = '/ProjectAkhir/public_html/';
               </div>
             </div>
             <hr class="nav-dropdown-divider" />
-            <a href="#" class="nav-dropdown-item" role="menuitem">
+            <a href="<?php echo $base_url; ?>pages/users/dashboard.php" class="nav-dropdown-item" role="menuitem">
               <svg width="14" height="14" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
               Profil Saya
             </a>
